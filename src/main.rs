37c612c3d9f36c8a89d8b718088e374deb79274e
Robson William <r.willup@hotmail.com>
2020-10-27 21:36:16 -0300

    Create todos and adding to a file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use structopt::StructOpt;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
 
 /// Creates a new development task
 #[derive(StructOpt)]
@@ -9,5 +11,23 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
-    println!("Task: {}", args.task)
+    println!("Task: {}", args.task);
+    let todo: String = format!("{}\n",args.task);
+
+    let exists = std::path::Path::new("test.txt").exists();
+
+    if exists == false {
+        std::fs::File::create("test.txt").expect("Could not create file");
+    }
+    
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("test.txt")
+        .unwrap();
+    
+        if let Err(e) = writeln!(file, "{}", todo) {
+            eprintln!("Couldn't write to file: {}", e);
+        }
+
 }
